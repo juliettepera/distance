69afe4c66ca10266b13da25a095ed65d0be0bb50
Juliette Pera <jpera@eurydice.ia.unc.edu>
2013-07-30 15:20:56 -0400

    add 2 files

---
+++ distanceCompute.cxx
@@ -0,0 +1,17 @@
+#include "distanceCompute.h"
+
+
+void computeDistanceAtoB()
+{
+    std::cout << std::endl << "you want to compute the A to B distances " << std::endl;
+}
+
+void computeDistanceBtoA()
+{
+    std::cout << std::endl << "you want to compute the B to A distances " << std::endl;
+}
+
+void computeDistanceBoth()
+{
+    std::cout << std::endl << "you want to compute both distances " << std::endl;
+}
---
+++ distanceCompute.h
@@ -0,0 +1,17 @@
+#ifndef DISTANCECOMPUTE_H
+#define DISTANCECOMPUTE_H
+
+// Other Libraries
+#include <string>
+#include <iostream>
+
+// My librairies
+#include "distanceGui.h"
+#include "distanceDisplay.h"
+
+void computeDistanceAtoB();
+void computeDistanceBtoA();
+void computeDistanceBoth();
+
+
+#endif
